3ae777b227887ee505ef5b05b2df8537fefcaf13
VIKAS SALARIA <engg.vikassalaria@gmail.com>
2021-10-22 00:17:53 +0530

    stage 02 changes

---
+++ src/stage_02_split_data.py
@@ -0,0 +1,66 @@
+from typing import Mapping
+
+from src.utils.all_utils import create_directory, read_yaml,save_local_df
+import argparse
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def split_and_save(config_path,params_path):
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+
+    #print(config)
+    
+    
+
+    #print(df.head())
+
+    # save dataset in the local directory
+    # crete path to directory: artifacts/raw_local_dir/data.csv
+
+    artifacts_dir = config["artifacts"] ['artifacts_dir']
+    raw_local_dir = config["artifacts"] ['raw_local_dir']
+    raw_local_file = config["artifacts"] ['raw_local_file']
+
+    raw_local_file_path = os.path.join(artifacts_dir,raw_local_dir,raw_local_file)
+    df = pd.read_csv(raw_local_file_path)
+
+    split_ratio = params["base"]["test_size"]
+    random_state = params["base"] ["random_state"]
+
+    train,test = train_test_split(df,test_size=split_ratio,random_state=random_state)
+
+    split_data_dir = config["artifacts"]["split_data_dir"]
+
+    create_directory([os.path.join(artifacts_dir, split_data_dir)])
+
+    train_data_filename = config["artifacts"]["train"]
+
+    
+    test_data_filename = config["artifacts"]["test"]
+
+    train_data_path = os.path.join(artifacts_dir,split_data_dir,train_data_filename)
+    test_data_path = os.path.join(artifacts_dir,split_data_dir,test_data_filename)
+
+    for data, data_path in (train,train_data_path), (test,test_data_path):
+        save_local_df(data,data_path)
+
+
+
+    #print(df.head())
+
+
+
+if __name__ == '__main__' :
+    args = argparse.ArgumentParser()
+
+    args.add_argument("--config","-c",default="config/config.yaml")
+    args.add_argument("--params","-p",default="params.yaml")
+   
+
+    parsed_args = args.parse_args()
+    print(parsed_args)
+
+    split_and_save(config_path=parsed_args.config,params_path = parsed_args.params)
+
--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -14,4 +14,8 @@ def create_directory(dirs:list):
         os.makedirs(dir_path,exist_ok=True)
         print(f"directory is created at {dir_path}")
 
+def save_local_df(data,data_path,index_status = False):
+    data.to_csv(data_path,index = index_status)
+    print(f"data is saved at {data_path}")
+
 
